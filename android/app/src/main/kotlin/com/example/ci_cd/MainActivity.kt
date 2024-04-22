5b5efba7f23d089373faf0d4c2316d3e3a8a9448
origin <islam.kamal@eg.aictec.com>
2024-04-22 12:45:40 +0200

    deploy with github actions v3

--- android/app/src/main/kotlin/com/example/ci_cd/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ci_cd/MainActivity.kt
@@ -1,6 +1,33 @@
-package com.example.ci_cd
+package com.example.flutter_app_firebase_feedback_demo
 
+import androidx.annotation.NonNull
+import com.google.firebase.appdistribution.InterruptionLevel
+import com.google.firebase.appdistribution.ktx.appDistribution
+import com.google.firebase.ktx.Firebase
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.example.flutter_app_firebase_feedback_demo/feedback_notification",
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "show") {
+                showFeedbackNotification()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    fun showFeedbackNotification() {
+        Firebase.appDistribution.showFeedbackNotification(
+            "Thanks for your feedback! (islam )",
+            InterruptionLevel.HIGH
+        )
+    }
 }
